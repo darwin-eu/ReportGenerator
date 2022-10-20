ac2ca1f7664758fd2004ec07fbbfbd6947a0e18f
CESAR BARBOZA <113351542+cbarbozaerasmus@users.noreply.github.com>
2022-10-20 14:36:33 +0200

    initial configuration

--- R/reportIncidencePrevalence.R
+++ R/reportIncidencePrevalence.R
@@ -1,18 +1,9 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-reportIncidencePrevalence <- function() {
+#' Report generator for IncidencePrevalence package
+#'
+#' @param x A tibble
+#' @export
+#' @import dplyr CDMConnector
+#' @return An HTML document
+reportIncidencePrevalence <- function(x) {
   print("Hello, world!")
 }
