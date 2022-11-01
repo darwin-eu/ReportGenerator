8f7aff1cdfbcec3add9b03d6fc5b64d1b00b4974
CESAR BARBOZA <113351542+cbarbozaerasmus@users.noreply.github.com>
2022-11-01 10:56:04 +0100

    Function with Office R

--- R/reportIncidencePrevalence.R
+++ R/reportIncidencePrevalence.R
@@ -15,18 +15,47 @@ reportIncidencePrevalence <- function(studyTitle,
                                       prevalenceData,
                                       word = TRUE) {
 
+  prevalenceTable <- prevalenceData$prevalence_estimates %>% select(time,
+                                                                    numerator,
+                                                                    denominator,
+                                                                    prev)
+
+  prevalenceGraph <- prevalenceData$prevalence_estimates %>%
+    left_join(prevalenceData$analysis_settings,
+              by = "prevalence_analysis_id") %>%
+    left_join(denominator$denominator_settings,
+              by = c("cohort_id_denominator_pop" = "cohort_definition_id")) %>%
+    ggplot(aes(start_time, prev))+
+    facet_grid(age_strata ~ sex_strata)+
+    geom_bar(stat='identity') +
+    scale_y_continuous(labels = scales::percent,
+                       limits = c(0,NA))+
+    theme_bw()
+
+  prevalenceDoc <- read_docx(path = paste0(system.file(package = "IncidencePrevalenceReport"),
+                                       "/rmarkdown/templates/IncidencePrevalenceReport.docx")) %>%
+    body_add_table(head(prevalenceTable, n = 20), style = "Normal Table") %>%
+    body_add_break() %>%
+    body_add_gg(value = prevalenceGraph, style = "Normal")
+
+
+  print(prevalenceDoc, target = here("Reports/IncidencePrevalenceReport.docx"))
+
   if (word == TRUE) {
     if (!file.exists(here("Reports/"))) {
       dir.create("Reports/")
     }
-    rmarkdown::render(
-      input = paste0(system.file(package = "IncidencePrevalenceReport"),
-                     "/rmarkdown/templates/IncidencePrevalenceReport.Rmd"),
-      output_format = word_document(reference_docx = paste0(system.file(package = "IncidencePrevalenceReport"),
-                                                            "/rmarkdown/templates/IncidencePrevalenceReport.docx")),
-      output_file = here("Reports/IncidencePrevalenceReport"),
-      encoding = 'UTF-8'
-    )
+    ## Renders docx with officeR
+
+    ## Renders docx with rmarkdown function
+    # rmarkdown::render(
+    #   input = paste0(system.file(package = "IncidencePrevalenceReport"),
+    #                  "/rmarkdown/templates/IncidencePrevalenceReport.Rmd"),
+    #   output_format = word_document(reference_docx = paste0(system.file(package = "IncidencePrevalenceReport"),
+    #                                                         "/rmarkdown/templates/IncidencePrevalenceReport.docx")),
+    #   output_file = here("Reports/IncidencePrevalenceReport"),
+    #   encoding = 'UTF-8'
+    # )
   } else {
     if (!file.exists(here("Reports/"))) {
       dir.create("Reports")
