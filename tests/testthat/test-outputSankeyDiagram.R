b230f2833329e8f5048cae74a2eb73b78973739b
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-03-08 13:27:38 +0100

    Added unit testing for sankey

---
+++ tests/testthat/test-outputSankeyDiagram.R
@@ -0,0 +1,30 @@
+library(testthat)
+library(ReportGenerator)
+
+# Dummy data
+data <- data.frame(
+  event_cohort_name1 = c(
+    "Acetaminophen", "Aspirin", "Amoxicillin+Clavulanate", "Acetaminophen",
+    "Aspirin", "Aspirin", "Acetaminophen"),
+  event_cohort_name2 = c(
+    NA, NA, NA, "Amoxicillin+Clavulanate", "Acetaminophen",
+    "Amoxicillin+Clavulanate", "Aspirin"),
+  freq = c(206, 211, 48, 6, 12, 6, 14))
+
+test_that("void", {
+  expect_error(outputSankeyDiagram())
+})
+
+
+test_that("minimal", {
+  # Make tmpDir
+  tmpDir <- paste0(tempdir(), "\\")
+
+  outputSankeyDiagram(
+    data = data,
+    outputFolder = tmpDir,
+    groupCombinations = TRUE,
+    fileName = "sankeyDiagram.html")
+
+  expect_true(file.exists(paste0(tmpDir, "\\sankeyDiagram.html")))
+})
