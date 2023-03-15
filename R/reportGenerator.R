fc046a479d4321d533a420d6b24d29a69e259a12
CESAR BARBOZA <113351542+cbarbozaerasmus@users.noreply.github.com>
2023-03-15 12:35:30 +0100

    spaces

--- R/reportGenerator.R
+++ R/reportGenerator.R
@@ -79,7 +79,7 @@ reportGenerator <- function() {
           # tags$p("input$objectsList1"),
           # verbatimTextOutput("results_1"),
           # tags$p("Tables and figures in the report"),
-          
+
           tableOutput("table"),
 
 
@@ -327,6 +327,7 @@ if(getRversion() >= "2.15.1")    utils::globalVariables(c("incidence_attrition",
 #'
 #' @return a dataframe with the properties of the items
 getItemsList <- function(uploadedFiles) {
+
   itemsList <- read.csv(system.file("config/itemsConfig.csv", package = "ReportGenerator"), sep = ";") %>%
     dplyr::mutate(signature = paste0(name, "(", arguments, ")"))
 
