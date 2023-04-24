52c661a29c7bc4f572a19cb94841d678b12c2d40
Ger Inberg <ginberg@gmail.com>
2023-04-24 17:21:37 +0200

    add shinyWidgets again

--- R/reportGenerator.R
+++ R/reportGenerator.R
@@ -18,7 +18,7 @@
 #'
 #' `ReportGenerator()` launches the package's main app. The user can upload a zip folder, and the function detects what figures and tables are available to generate a Word report.
 #'
-#' @import dplyr rmarkdown here ggplot2 quarto shiny shinydashboard officer flextable
+#' @import dplyr rmarkdown here ggplot2 quarto shiny shinydashboard shinyWidgets officer flextable
 #' @importFrom sortable bucket_list add_rank_list
 #' @export
 reportGenerator <- function() {
@@ -143,10 +143,10 @@ reportGenerator <- function() {
     # Uploaded object: Incidence Attrition
     incidence_attrition <- reactive({
       result <- NULL
+      configColumns <- read.csv(system.file("config/variablesConfig.csv", package = "ReportGenerator")) %>%
+        dplyr::filter(name == "incidence_attrition")
+      configColumns <- configColumns$variables
       for (i in uploadedFiles()) {
-        configColumns <- read.csv(system.file("config/variablesConfig.csv", package = "ReportGenerator")) %>%
-          dplyr::filter(name == "incidence_attrition")
-        configColumns <- configColumns$variables
         csvData <- read_csv(i)
         resultsColumns <- names(csvData)
         if (length(configColumns) == length(resultsColumns)) {
@@ -161,10 +161,10 @@ reportGenerator <- function() {
     # Uploaded object: Prevalence Attrition
     prevalence_attrition <- reactive({
       result <- NULL
+      configColumns <- read.csv(system.file("config/variablesConfig.csv", package = "ReportGenerator")) %>%
+        dplyr::filter(name == "prevalence_attrition")
+      configColumns <- configColumns$variables
       for (i in uploadedFiles()) {
-        configColumns <- read.csv(system.file("config/variablesConfig.csv", package = "ReportGenerator")) %>%
-          dplyr::filter(name == "prevalence_attrition")
-        configColumns <- configColumns$variables
         csvData <- read_csv(i)
         resultsColumns <- names(csvData)
         if (length(configColumns) == length(resultsColumns)) {
@@ -179,10 +179,10 @@ reportGenerator <- function() {
     # Uploaded object: Incidence Estimates
     incidence_estimates <- reactive({
       result <- NULL
+      configColumns <- read.csv(system.file("config/variablesConfig.csv", package = "ReportGenerator")) %>%
+        dplyr::filter(name == "incidence_estimates")
+      configColumns <- configColumns$variables
       for (i in uploadedFiles()) {
-        configColumns <- read.csv(system.file("config/variablesConfig.csv", package = "ReportGenerator")) %>%
-          dplyr::filter(name == "incidence_estimates")
-        configColumns <- configColumns$variables
         csvData <- read_csv(i)
         resultsColumns <- names(csvData)
         if (length(configColumns) == length(resultsColumns)) {
