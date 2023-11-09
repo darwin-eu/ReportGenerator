72610453a7de0a4229279e36a1bee4aa60b5c8ae
Cesar Barboza <c.barbozagutierrez@erasmusmc.nl>
2023-11-09 02:06:21 +0100

    coverage test

--- tests/testthat/test-automaticText.R
+++ tests/testthat/test-automaticText.R
@@ -5,8 +5,8 @@ test_that("check automatic character single", {
 
 test_that("check automatic character two databases", {
 
-  incidence_attrition_test_two <- incidence_attrition_test %>% filter(cdm_name %in% c("CHUBX", "CPRD GOLD"))
-  prevalence_attrition_test_two <- prevalence_attrition_test %>% filter(cdm_name %in% c("CHUBX", "CPRD GOLD"))
+  incidence_attrition_test_two <- incidence_attrition_test %>% filter(cdm_name %in% c("CHUBX", "IPCI"))
+  prevalence_attrition_test_two <- prevalence_attrition_test %>% filter(cdm_name %in% c("CHUBX", "IPCI"))
   autotext <- table1aAutText(incidence_attrition = incidence_attrition_test_two, prevalence_attrition = prevalence_attrition_test_two)
   expect_type(autotext, "character")
 })
