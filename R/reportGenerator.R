208c79c10a220307ac4fb4a0a30bea5c91006df1
Ger Inberg <ginberg@gmail.com>
2024-03-11 17:03:34 +0100

    add spinner

--- R/reportGenerator.R
+++ R/reportGenerator.R
@@ -18,10 +18,9 @@
 #'
 #' `ReportGenerator()` launches the package's main app. The user can upload a zip folder, and the function detects what figures and tables are available to generate a Word report.
 #'
-#' @import dplyr shiny shinydashboard shinyWidgets shinycssloaders officer flextable waldo readr yaml googleVis TreatmentPatterns PatientProfiles
+#' @import dplyr shiny shinydashboard shinyWidgets shinycssloaders officer flextable waldo readr yaml TreatmentPatterns PatientProfiles
 #' @importFrom sortable bucket_list add_rank_list sortable_options
 #' @importFrom utils read.csv tail unzip
-#' @importFrom gtools mixedsort
 #' @importFrom ggplot2 ggsave
 #' @importFrom gto body_add_gt
 #' @importFrom here here
@@ -101,6 +100,8 @@ reportGenerator <- function() {
 
     # Check input data
     observeEvent(input$datasetLoad, {
+      shinycssloaders::showPageSpinner()
+
       # Read  file paths
       inFile <- input$datasetLoad
       fileDataPath <- inFile$datapath
@@ -137,6 +138,7 @@ reportGenerator <- function() {
         itemsList$objects[["items"]] <- c(itemsList$objects[["items"]], getItemsList(items))
       }
       unlink(csvLocation, recursive = TRUE)
+      shinycssloaders::hidePageSpinner()
     })
 
     # Reset and back to initial tab
@@ -431,16 +433,6 @@ reportGenerator <- function() {
         DT::datatable(dataReportFrame, options = list(dom = 't'))
       }
     })
-
-    # To check data in report:
-
-    # output$dataReportMenu <- renderPrint({
-    #   # dataReport
-    #   dataReportList <- reactiveValuesToList(dataReport)
-    #   dataReportList
-    #   # length(dataReportList) == 0
-    #   # objectsListPreview()
-    # })
 
     # Word report generator
     output$generateReport <- downloadHandler(
