70744b274bcc904936ae8da3c8c4008f9eb515fd
Cesar Barboza <ce.barboza@pm.me>
2025-02-20 16:28:35 +0100

    document

--- R/reportGenerator.R
+++ R/reportGenerator.R
@@ -20,10 +20,9 @@
 #'
 #' @param logger optional logger object
 #'
-#' @import dplyr shiny shinydashboard shinyWidgets shinycssloaders officer flextable waldo readr yaml TreatmentPatterns PatientProfiles ggplot2
+#' @import dplyr shiny shinydashboard shinyWidgets shinycssloaders officer ggplot2 flextable waldo readr yaml TreatmentPatterns PatientProfiles ggplot2
 #' @importFrom sortable bucket_list add_rank_list sortable_options
 #' @importFrom utils read.csv tail unzip
-#' @importFrom ggplot2 ggsave aes geom_bar theme_minimal theme axis.text.x element_text
 #' @importFrom gto body_add_gt
 #' @importFrom here here
 #' @importFrom TreatmentPatterns createSankeyDiagram
