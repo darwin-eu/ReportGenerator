09b9c43f6e427e7bda5a60245d7e16a1c1b0183d
Cesar Barboza <ce.barboza@pm.me>
2025-03-30 23:32:14 +0200

    delete test

--- tests/testthat/test-attritionModules.R
+++ tests/testthat/test-attritionModules.R
@@ -1,21 +1,21 @@
-test_that("Prevalence Attrition", {
-
-  fileDataPath <- list.files(test_path("studies", "summarised_zip"),
-                             pattern = "zip",
-                             full.names = TRUE)
-  test_data <- joinDatabases(fileDataPath[1])
-
-  prevalence_attrition <- test_data$summarised_result %>%
-    visOmopResults::filterSettings(result_type == "prevalence_attrition") %>%
-    dplyr::filter(cdm_name %in% "CHUBX") %>%
-    visOmopResults::filterSettings(denominator_sex %in% "Female",
-                                   denominator_age_group %in% "0 to 64")
-
-  expect_error(tablePrevalenceAttrition(result = prevalence_attrition,
-                           type = "gt",
-                           header = "variable_name",
-                           groupColumn = c("cdm_name", "variable_level"),
-                           settingsColumn = NULL,
-                           hide = "estimate_name"))
-
-})
+# test_that("Prevalence Attrition", {
+#
+#   fileDataPath <- list.files(test_path("studies", "summarised_zip"),
+#                              pattern = "zip",
+#                              full.names = TRUE)
+#   test_data <- joinDatabases(fileDataPath[1])
+#
+#   prevalence_attrition <- test_data$summarised_result %>%
+#     visOmopResults::filterSettings(result_type == "prevalence_attrition") %>%
+#     dplyr::filter(cdm_name %in% "CHUBX") %>%
+#     visOmopResults::filterSettings(denominator_sex %in% "Female",
+#                                    denominator_age_group %in% "0 to 64")
+#
+#   expect_error(tablePrevalenceAttrition(result = prevalence_attrition,
+#                            type = "gt",
+#                            header = "variable_name",
+#                            groupColumn = c("cdm_name", "variable_level"),
+#                            settingsColumn = NULL,
+#                            hide = "estimate_name"))
+#
+# })
